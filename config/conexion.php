39011559eaca6dd35f8e642ca89df30e429a6c34
J.P. ospina C. <ospinacrespoj@gmail.com>
2026-03-04 19:16:27 -0500

    correccion

--- config/conexion.php
+++ config/conexion.php
@@ -1,14 +1,17 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "";
+$pass = ""; 
 $db   = "bd_inventario";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
-}header.php
-?>
+}
 
+// Configurar el conjunto de caracteres a UTF-8 para evitar errores con tildes
+$conn->set_charset("utf8");
+?>
 
